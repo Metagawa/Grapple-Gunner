791c1e45575b1a9f8c5992b0ae889c15982943e5
TomC17 <tommiecarey17@gmail.com>
2021-11-08 03:47:31 +0000

    Fixed grappler physics, added new grappler modules and swords

--- Assets/_input/Scripts/Handlers/SlowGrapple.cs
+++ Assets/_input/Scripts/Handlers/SlowGrapple.cs
@@ -1,38 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class SlowGrapple : MonoBehaviour {
+
+public class SlowGrapple : MonoBehaviour
+{
+    [SerializeField] private InputActionReference grappleActionReference;
 
     private LineRenderer lr;
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
-    public Transform gunTip, camera, player;
+    public Transform origin, target, player;
     private float maxDistance = 100f;
     private SpringJoint joint;
 
-    void Awake() {
+    void Awake()
+    {
         lr = GetComponent<LineRenderer>();
     }
 
-    void Update() {
-       /// if (Input.GetMouseButtonDown(0)) {
-       ///     StartGrapple();
-       /// }
-       /// else if (Input.GetMouseButtonUp(0)) {
-       ///     StopGrapple();
-       /// }
+    void Start()
+    {
+        ///grappleActionReference.action.performed += StopGrapple;
+
+        grappleActionReference.action.started += StartGrapple;
+        grappleActionReference.action.canceled += StopGrapple;
+
+    }
+    void Update()
+    {
+        /// if (Input.GetMouseButtonDown(0)) {
+        ///     StartGrapple();
+        /// }
+        /// else if (Input.GetMouseButtonUp(0)) {
+        ///     StopGrapple();
+        /// }
     }
 
     //Called after Update
-    void LateUpdate() {
+    void LateUpdate()
+    {
         DrawRope();
     }
 
     /// <summary>
     /// Call whenever we want to start a grapple
     /// </summary>
-   public void StartGrapple() {
+    void StartGrapple(InputAction.CallbackContext obj)
+    {
         RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable)) {
+        if (Physics.Raycast(target.position, target.forward, out hit, maxDistance, whatIsGrappleable))
+        {
             grapplePoint = hit.point;
             joint = player.gameObject.AddComponent<SpringJoint>();
             joint.autoConfigureConnectedAnchor = false;
@@ -42,18 +59,19 @@ public class SlowGrapple : MonoBehaviour {
 
             //The distance grapple will try to keep from grapple point. 
             ///joint.maxDistance = distanceFromPoint * 0.2f;
-           /// joint.minDistance = distanceFromPoint * 0.25f;
-            joint.maxDistance = distanceFromPoint * 0.8f;
-            joint.minDistance = distanceFromPoint * 0.25f;
+            /// joint.minDistance = distanceFromPoint * 0.25f;
+            joint.maxDistance = distanceFromPoint * 0.6f;
+            joint.minDistance = distanceFromPoint * 0.5f;
 
             //Adjust these values to fit your game.
-            joint.spring = 6f;
+            joint.spring = 4.5f;
             joint.damper = 7f;
-            joint.massScale = 6f;
+            joint.massScale = 4.5f;
+
 
 
             lr.positionCount = 2;
-            currentGrapplePosition = gunTip.position;
+            currentGrapplePosition = origin.position;
         }
     }
 
@@ -61,28 +79,33 @@ public class SlowGrapple : MonoBehaviour {
     /// <summary>
     /// Call whenever we want to stop a grapple
     /// </summary>
-    public void StopGrapple() {
+    void StopGrapple(InputAction.CallbackContext obj)
+    {
         lr.positionCount = 0;
         Destroy(joint);
     }
 
+
     private Vector3 currentGrapplePosition;
-    
-    void DrawRope() {
+
+    void DrawRope()
+    {
         //If not grappling, don't draw rope
         if (!joint) return;
 
         currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
-        
-        lr.SetPosition(0, gunTip.position);
+
+        lr.SetPosition(0, origin.position);
         lr.SetPosition(1, currentGrapplePosition);
     }
 
-    public bool IsGrappling() {
+    public bool IsGrappling()
+    {
         return joint != null;
     }
 
-    public Vector3 GetGrapplePoint() {
+    public Vector3 GetGrapplePoint()
+    {
         return grapplePoint;
     }
 }
